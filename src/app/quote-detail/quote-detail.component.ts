5ad76be9e288dbd16dc9fcf7097ade4d3030762c
Ecline Koech <eclinejerutokoech@gmail.com>
2021-10-12 01:38:58 +0300

    generate quote details component

---
+++ src/app/quote-detail/quote-detail.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Quotes } from '../quotes';
+
+@Component({
+  selector: 'app-quote-detail',
+  templateUrl: './quote-detail.component.html',
+  styleUrls: ['./quote-detail.component.css'],
+})
+export class QuoteDetailComponent implements OnInit {
+  @Input() quote!: Quotes;
+  @Output() isComplete = new EventEmitter<boolean>();
+
+  quoteDelete(complete: boolean) {
+    this.isComplete.emit(complete);
+  }
+
+  upvote() {
+    this.quote.likes += 1;
+  }
+  downvote() {
+    this.quote.dislikes += 1;
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
